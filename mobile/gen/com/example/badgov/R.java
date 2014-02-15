ce6d26bcc1c87df1c238525cd597764d0e6f00e9
Razan <snow_rose185@hotmail.com>
2014-02-15 16:17:36 +0200

    Main Page

--- mobile/gen/com/example/badgov/R.java
+++ mobile/gen/com/example/badgov/R.java
@@ -8,6 +8,9 @@
 package com.example.badgov;
 
 public final class R {
+    public static final class array {
+        public static final int tweets=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,24 +25,24 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
+        public static final int temp=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
-        public static final int badServiceRadio=0x7f08000c;
-        public static final int btnCapture=0x7f080000;
-        public static final int category=0x7f08000b;
-        public static final int corruptionServiceRadio=0x7f08000d;
-        public static final int dateTextView=0x7f080008;
-        public static final int imageView1=0x7f080006;
-        public static final int locationTxtView=0x7f08000a;
-        public static final int reviewTxt=0x7f080007;
-        public static final int skipImage=0x7f080001;
-        public static final int submitBtn=0x7f08000e;
-        public static final int tableRow1=0x7f080002;
-        public static final int tableRow2=0x7f080004;
-        public static final int tableRow3=0x7f080005;
-        public static final int textView1=0x7f080003;
-        public static final int whereEditTxt=0x7f080009;
+        public static final int action_settings=0x7f09000d;
+        public static final int badServiceRadio=0x7f09000a;
+        public static final int btnCapture=0x7f090000;
+        public static final int category=0x7f090009;
+        public static final int corruptionServiceRadio=0x7f09000b;
+        public static final int dateTextView=0x7f090006;
+        public static final int imageView1=0x7f090004;
+        public static final int label=0x7f090003;
+        public static final int locationTxtView=0x7f090008;
+        public static final int reviewTxt=0x7f090005;
+        public static final int skipImage=0x7f090001;
+        public static final int status=0x7f090002;
+        public static final int submitBtn=0x7f09000c;
+        public static final int whereEditTxt=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -48,7 +51,7 @@ public final class R {
         public static final int splash_screen=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int Snap=0x7f050004;
@@ -80,10 +83,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
