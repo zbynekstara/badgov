e895c1c8100a07b8be5ab59bfc36304001284f70
David Hu <octopie@gmail.com>
2014-02-15 19:00:27 +0400

    right token

--- web/app/controllers/TestDataController.php
+++ web/app/controllers/TestDataController.php
@@ -10,7 +10,7 @@ class TestDataController extends BaseController {
   public function getTwitterTest()
   {
     $settings = array(
-      'oauth_access_token' => "jftihe71X1Ls4uveYEiLpEFxTHidCJh1n3QhXl1",
+      'oauth_access_token' => "2344979191-jftihe71X1Ls4uveYEiLpEFxTHidCJh1n3QhXl1",
       'oauth_access_token_secret' => "rWFgwYIJQS4cxvICKvcSEovMw3pqIyePuIwRIlZ493eaI",
       'consumer_key' => "aSvTZCvgSNfa6kkj41RHQ",
       'consumer_secret' => "JUBsyweHzviiVy6TirESrMgov9I0qeNYBQPLWf5mRQ"
