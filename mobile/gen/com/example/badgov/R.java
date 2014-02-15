62cddc1e8de0be70ac70fc78e16f3584ef2407c2
Razan <snow_rose185@hotmail.com>
2014-02-15 09:36:01 +0200

    SplashScreen

--- mobile/gen/com/example/badgov/R.java
+++ mobile/gen/com/example/badgov/R.java
@@ -24,13 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
         public static final int btnCapture=0x7f080000;
         public static final int skipImage=0x7f080001;
+        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int report=0x7f030001;
+        public static final int splash_screen=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -41,6 +43,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int logo=0x7f050005;
     }
     public static final class style {
         /** 
